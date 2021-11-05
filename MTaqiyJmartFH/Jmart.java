ec7eeaa6497c630a08a44703228e74f2f3a84e53
TaqiyNur <muhammad.taqiy@ui.ac.id>
2021-11-06 00:02:48 +0700

    Modul 6 - Muhammad Taqiy Nur Furqon - Tugas Pendahuluan

--- MTaqiyJmartFH/Jmart.java
+++ MTaqiyJmartFH/Jmart.java
@@ -1,5 +1,10 @@
 package MTaqiyJmartFH;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import com.google.gson.*;
 
 /**
  * Write a description of class Jmart here.
@@ -9,8 +14,26 @@ package MTaqiyJmartFH;
  */
 public class Jmart
 {
+	class Country {
+		public String name;
+		public int population;
+		public List<String> listOfStates;
+	}
+	
    public static void main(String[] args) {
-     
+     String filepath = "/Users/mtaqi/Documents/Praktikum OOP/jmart/city.json";
+     Gson gson = new Gson();
+     try {
+    	 BufferedReader br = new BufferedReader(new FileReader(filepath));
+    	 Country input = gson.fromJson(br,  Country.class);
+    	 System.out.println("name: " + input.name);
+    	 System.out.println("population: " + input.population);
+    	 System.out.println("States: ");
+    	 input.listOfStates.forEach(state -> System.out.println(state));
+     }
+    catch (IOException e) {
+    	e.printStackTrace();
+    }
    }
    
 }
